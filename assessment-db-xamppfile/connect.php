2b417a3e792f357abd275308fe2a2a4bcd869e6c
Nemuel <lim.nemuel.22@gmail.com>
2020-03-03 21:22:50 +0800

    Update connect.php file

--- assessment-db-xamppfile/connect.php
+++ assessment-db-xamppfile/connect.php
@@ -1,8 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST ,OPTIONS, PUT');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Content-Type: application/json');
 // Connect with the database.
 function connect()
 {
